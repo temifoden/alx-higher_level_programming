f52353f4cf16ef4337bcd9c08082eb86e8762d55
Temitayo <temfoden@gmail.com>
2024-05-24 04:34:53 +0100

    add script that print in an order

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records from the table second_table, displaying score and name,
+-- ordered by score in descending order
+SELECT score, name FROM second_table ORDER BY score DESC;
