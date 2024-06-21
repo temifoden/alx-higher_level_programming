e92b9e4a5648dbfc3383f366bbb0a0483f9e734f
Temitayo <temfoden@gmail.com>
2024-06-21 04:14:22 +0100

    add: function that reverse a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,3 +1,11 @@
 exports.nbOccurences = function (list, searchElement) {
   return list.reduce((count, element) => count + (element === searchElement ? 1 : 0), 0);
 };
+exports.esrever = function (list) {
+    const reversedList = [];
+    for (let i = list.length - 1; i >= 0; i--) {
+      reversedList.push(list[i]);
+    }
+    return reversedList;
+  };
+  
