3947aa560c8e5e3f06bcdda98b9add533a71ce33
Temitayo <temfoden@gmail.com>
2024-06-27 22:28:46 +0100

    update script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     # Connect to the mysql database
-    db = MySQLdb.connect(host='localhost', port=3306, user=mysql_username, passwd=mysql_pswd, db=db_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_pswd, db=db_name)
 
     # Create a cursor object
     cursor = db.cursor()
