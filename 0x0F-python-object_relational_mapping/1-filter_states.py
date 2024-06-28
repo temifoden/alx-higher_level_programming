0842d4f71beab73756d7caf28331b5ecc353f542
Temitayo <temfoden@gmail.com>
2024-06-28 02:29:45 +0100

    add script that fetch all state starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Retrieve command-line argument
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # Connect to the mysql database
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=mysql_username, passwd=mysql_password, db=db_name)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query to select all states order by ID
+    cursor.execute('SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC')
+
+    # Fetch all rows from the executed query
+    states = cursor.fetchall()
+
+    # print each state
+    for state in states:
+        print(state)
+
+    # close the cursor and database connection
+    cursor.close()
+    # disconnect database
+    db.close()
