b83ec64a7ed497ac637c19a6e4ccbbe0114306fc
Temitayo <temfoden@gmail.com>
2024-07-30 17:44:39 +0100

    update script

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.readFile(process.argv[2], process.argv.slice(3).join(' '), 'utf8', function (error) {
-  if (error){
-    console.error(error);
-    return;
-  }
-  console.log("File written successfully");
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.error(error);
 });
